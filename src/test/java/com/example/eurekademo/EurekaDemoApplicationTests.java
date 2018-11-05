dc3d49584332883368e2fe5c7246005d7f0cbbc1
mengdai <mengdai@nothing.attdns.com>
2018-11-05 11:12:27 +0800

    添加测试类注解

--- src/test/java/com/example/eurekademo/EurekaDemoApplicationTests.java
+++ src/test/java/com/example/eurekademo/EurekaDemoApplicationTests.java
@@ -2,11 +2,15 @@ package com.example.eurekademo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = EurekaDemoApplication.class)
+@WebAppConfiguration
+@AutoConfigureMockMvc
 public class EurekaDemoApplicationTests {
 
 	@Test
